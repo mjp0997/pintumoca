ccec82a5ae62854c171763183e9c575eac657e54
Mario José Pineda <72577859+mjp0997@users.noreply.github.com>
2023-06-05 23:47:31 -0400

    404 component

---
+++ resources/views/components/ui/404.blade.php
@@ -0,0 +1,14 @@
+<div class="mt-4 mx-auto component-404">
+   <h1 class="text-warning h1-404">404</h1>
+
+   <div class="d-flex flex-column">
+      <h3><i class="fas fa-exclamation-triangle text-warning"></i> Ups! Página no encontrada.</h3>
+
+      <p class="mb-0">
+         No pudimos encontrar la página que estabas buscando.
+         <br>
+         Puede que quieras <a href="{{ route('home')}}">volver al inicio</a>.
+      </p>
+   </div>
+   <!-- /.error-content -->
+</div>
