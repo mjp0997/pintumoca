b2e6ee979e5068d2ec0696b34782a882bc2c2612
Mario José Pineda <72577859+mjp0997@users.noreply.github.com>
2023-06-16 21:23:08 -0400

    Relaciones y atributos on the fly de venta

--- app/Models/SaleLine.php
+++ app/Models/SaleLine.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SaleLine extends Model
@@ -12,5 +13,39 @@ class SaleLine extends Model
 
     protected $table = 'sale_line';
 
-    protected $fillable = ['sale_id', 'stock_id', 'price', 'quantity'];
+    protected $fillable = ['sale_id', 'product_id', 'price', 'quantity'];
+
+    protected $appends = ['subtotal'];
+
+
+
+    // Appended attributes
+
+    public function getSubtotalAttribute() {
+        return number_format($this->price * $this->quantity, 2);
+    }
+
+
+
+    // Relationships
+
+    /**
+     * Get the sale that owns the SaleLine
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function sale(): BelongsTo
+    {
+        return $this->belongsTo(Sale::class, 'sale_id');
+    }
+
+    /**
+     * Get the product that owns the SaleLine
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
--- app/Models/SalePayment.php
+++ app/Models/SalePayment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SalePayment extends Model
@@ -12,5 +13,63 @@ class SalePayment extends Model
 
     protected $table = 'sale_payment';
 
-    protected $fillable = ['sale_id', 'currency_id', 'payment_id', 'amount'];
+    protected $fillable = ['sale_id', 'currency_id', 'payment_id', 'amount', 'dollar_rate'];
+
+    protected $appends = ['dollars_amount', 'formated_amount'];
+
+
+
+    // Appended attributes
+
+    public function getDollarsAmountAttribute()
+    {
+        if ($this->currency->name == 'bolívares') {
+            return number_format($this->amount / $this->dollar_rate, 2);
+        }
+
+        return number_format($this->amount, 2);
+    }
+
+    public function getFormatedAmountAttribute()
+    {
+        $currency = number_format($this->amount, 2);
+
+        $unit = $this->currency->iso_code;
+
+        return "$currency $unit";
+    }
+
+
+
+    // Relationships
+
+    /**
+     * Get the sale that owns the SalePayment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function sale()
+    {
+        return $this->belongsTo(Sale::class, 'sale_id');
+    }
+
+    /**
+     * Get the payment method that owns the SalePayment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function paymentMethod()
+    {
+        return $this->belongsTo(PaymentMethod::class, 'payment_id');
+    }
+
+    /**
+     * Get the currency that owns the SalePayment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function currency(): BelongsTo
+    {
+        return $this->belongsTo(Currency::class, 'currency_id');
+    }
 }
